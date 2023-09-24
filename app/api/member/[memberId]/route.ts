e7723536c44e919ba3b7b9c227e30a969c84fef0
joseph0926 <joseph0926@naver.com>
2023-09-25 08:54:50 +0900

    #34 유저 벤 로직

--- app/api/member/[memberId]/route.ts
+++ app/api/member/[memberId]/route.ts
@@ -57,3 +57,53 @@ export const PATCH = async (req: Request, { params }: { params: { memberId: stri
     return new NextResponse('Server Error', { status: 500 });
   }
 };
+
+export const DELETE = async (req: Request, { params }: { params: { memberId: string } }) => {
+  try {
+    const user = await getCurrentUser();
+    const { searchParams } = new URL(req.url);
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const serverId = searchParams.get('serverId');
+    if (!serverId) {
+      return new NextResponse('Server ID is missing', { status: 400 });
+    }
+    if (!params.memberId) {
+      return new NextResponse('Member Id is missing', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        userId: user.id
+      },
+      data: {
+        members: {
+          deleteMany: {
+            id: params.memberId,
+            userId: {
+              not: user.id
+            }
+          }
+        }
+      },
+      include: {
+        members: {
+          include: {
+            user: true
+          },
+          orderBy: {
+            role: 'asc'
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log('[MEMBER_DELETE]', error);
+    return new NextResponse('Server Error', { status: 500 });
+  }
+};
