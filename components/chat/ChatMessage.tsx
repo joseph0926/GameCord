6a698a63cc1a09340d7113fc68d220ea18044106
joseph0926 <joseph0926@naver.com>
2023-09-28 17:08:59 +0900

    #47 실시간채팅 세팅

--- components/chat/ChatMessage.tsx
+++ components/chat/ChatMessage.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Fragment, useRef, ElementRef } from 'react';
+import { Fragment, useRef, ElementRef, useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import { Member, Message, User } from '@prisma/client';
 import { Loader2, ServerCrash } from 'lucide-react';
 import ChatWelcome from './ChatWelcome';
 import ChatItem from './ChatItem';
+import { pusherClient, toPusherKey } from '@/lib/pusher';
 
 const DATE_FORMAT = 'd MMM yyyy, HH:mm';
 
@@ -19,13 +20,31 @@ interface ChatMessagesProps {
   name: string;
   member: Member;
   chatId: string;
+  initialMessage: Message[];
   apiUrl: string;
   paramKey: 'channelId' | 'groupMessageId';
   paramValue: string;
   type: 'channel' | 'groupMessage';
 }
 
-const ChatMessages = ({ name, member, chatId, apiUrl, paramKey, paramValue, type }: ChatMessagesProps) => {
+const ChatMessages = ({ name, member, chatId, apiUrl, paramKey, initialMessage, paramValue, type }: ChatMessagesProps) => {
+  const [message, setMessage] = useState<Message[]>(initialMessage);
+
+  useEffect(() => {
+    pusherClient.subscribe(toPusherKey(`chat:${chatId}`));
+
+    const messageHandler = (message: Message) => {
+      setMessage((prevState) => [message, ...prevState]);
+    };
+
+    pusherClient.bind('incoming-message', messageHandler);
+
+    return () => {
+      pusherClient.unsubscribe(toPusherKey(`chat:${chatId}`));
+      pusherClient.unbind('incoming-message', messageHandler);
+    };
+  }, []);
+
   return <div className="flex flex-1 flex-col overflow-y-auto py-4"></div>;
 };
 
