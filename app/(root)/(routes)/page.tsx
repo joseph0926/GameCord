260d143b1d52849e461902cf19be9a2fd1540b75
joseph0926 <joseph0926@naver.com>
2023-10-04 08:59:08 +0900

    url fix

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -25,7 +25,7 @@ const MainPage = async () => {
 
       await db.server.update({
         where: {
-          id: 'f8c9a28f-4e07-4257-bdd2-08d15f5cc5ea'
+          id: '4a80a75b-84b7-4f8a-a9fc-706b44d79bdf'
         },
         data: {
           members: {
@@ -44,7 +44,10 @@ const MainPage = async () => {
     <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center gap-4">
       <h1 className="text-2xl">현재 Server 페이지 이외 페이지들은 개발중에 있습니다.</h1>
       <p className="text-xl">아래 테스트 서버로 접속하시거나 서버를 직접 생성해주세요.</p>
-      <Link href={`/server/f8c9a28f-4e07-4257-bdd2-08d15f5cc5ea`} className="bg-primary-500 px-4 py-2 text-white">
+      <Link
+        href={`/server/4a80a75b-84b7-4f8a-a9fc-706b44d79bdf/channel/0d040143-0906-4a1d-98d2-9579d5594c64`}
+        className="bg-primary-500 px-4 py-2 text-white"
+      >
         TestServer
       </Link>
     </div>
