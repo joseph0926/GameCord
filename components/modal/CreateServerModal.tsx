6a2102fe68cd3ee3575885ca24ce59eb92afba25
joseph0926 <joseph0926@naver.com>
2023-10-03 10:43:22 +0900

    #70 modal 추가

---
+++ components/modal/CreateServerModal.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { serverFormSchema } from '@/lib/validations/server';
+import { Button } from '../ui/button';
+import FileUpload from '../ui/file-upload';
+import { createServer } from '@/lib/actions/server/mutateActions';
+import { useOrigin } from '@/hooks/useOrigin';
+import { useModalStore } from '@/hooks/useModalStore';
+
+const CreateServerModal = () => {
+  const origin = useOrigin();
+  const { isOpen, onClose, type } = useModalStore();
+
+  const isMoadlOpen = isOpen && type === 'createServer';
+
+  const form = useForm({
+    resolver: zodResolver(serverFormSchema),
+    defaultValues: {
+      name: '',
+      imageUrl: ''
+    }
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const submitHandler = async (values: z.infer<typeof serverFormSchema>) => {
+    await createServer({ data: { ...values, path: `${origin}/server` } });
+  };
+
+  const closeHandler = () => {
+    form.reset();
+    onClose();
+  };
+
+  return (
+    <Dialog open={isMoadlOpen} onOpenChange={closeHandler}>
+      <DialogContent className="overflow-hidden bg-white p-0 text-black">
+        <DialogHeader className="px-6 pt-8">
+          <DialogTitle className="text-center text-2xl">Create Your Server</DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus temporibus ratione quaerat quam, soluta,
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(submitHandler)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload endpoint="serverImage" value={field.value} onChange={field.onChange} />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-xs font-bold uppercase text-zinc-500 dark:text-secondary/70">Server Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="border-0 bg-zinc-300/50 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="bg-gray-100 px-6 py-4">
+              <Button disabled={isLoading} variant="primary">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateServerModal;
