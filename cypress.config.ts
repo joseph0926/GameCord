cbcbdddf0c0660cfe44cfb153353e603778da054
joseph.kim <joseph0926@naver.com>
2023-12-07 14:48:04 +0900

    #149 cypress 초기 설정

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+import path from "path";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+      webpackConfig: {
+        resolve: {
+          alias: {
+            "@components": path.resolve(__dirname, "./src/components"),
+          },
+        },
+      },
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,5 @@
+describe('home page', () => {
+  it('should render the main logo', () => {
+    cy.visit('http://localhost:3000');
+  });
+});
