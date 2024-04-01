56accdd533d23718a6ed9cf21b70f93a342b34c6
김영훈 <joseph0926@naver.com>
2024-04-01 09:46:19 +0900

    #154 clerk -> auth.js

---
+++ src/app/(auth)/components/signup-form.tsx
@@ -0,0 +1,141 @@
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { signupSchema } from '@/lib/validations/sign.schema';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+export default function SignupForm() {
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      name: ''
+    }
+  });
+
+  const submitHandler = (values: z.infer<typeof signupSchema>) => {
+    console.log(values);
+  };
+
+  const hasEmailError = form.formState.errors.email != undefined;
+  const hasPasswordError = form.formState.errors.password != undefined;
+  const hasNameError = form.formState.errors.name != undefined;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(submitHandler)}
+        className="flex w-full flex-col gap-10"
+      >
+        <div className="flex flex-col gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel
+                  className={cn(
+                    'font-medium',
+                    hasNameError ? 'text-destructive' : 'text-gray-100'
+                  )}
+                >
+                  Name
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="test01"
+                    type="text"
+                    className={cn(
+                      'bg-transparent text-gray-200 placeholder:text-gray-400',
+                      hasNameError ? 'border-destructive' : 'border-gray-200'
+                    )}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel
+                  className={cn(
+                    'font-medium',
+                    hasEmailError ? 'text-destructive' : 'text-gray-100'
+                  )}
+                >
+                  Email
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="test@example.com"
+                    type="text"
+                    className={cn(
+                      'bg-transparent text-gray-200 placeholder:text-gray-400',
+                      hasEmailError ? 'border-destructive' : 'border-gray-200'
+                    )}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel
+                  className={cn(
+                    'font-medium',
+                    hasPasswordError ? 'text-destructive' : 'text-gray-100'
+                  )}
+                >
+                  Password
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="******"
+                    type="password"
+                    className={cn(
+                      'bg-transparent text-gray-200 placeholder:text-gray-400',
+                      hasPasswordError
+                        ? 'border-destructive'
+                        : 'border-gray-200'
+                    )}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button
+          type="submit"
+          variant="outline"
+          size="lg"
+          className="bg-transparent font-semibold text-gray-100"
+          disabled={hasEmailError || hasPasswordError || hasNameError}
+        >
+          Signup
+        </Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/service/query/user.service.ts
@@ -0,0 +1,83 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { db } from '@/lib/db';
+
+import { Resend } from 'resend';
+import {
+  getPasswordResetTokenByEmail,
+  getVerificationTokenByEmail
+} from './auth.service';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `${domain}/auth/new-password?token=${token}`;
+
+  await resend.emails.send({
+    from: 'mail@auth-masterclass-tutorial.com',
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+  });
+};
+
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+
+  await resend.emails.send({
+    from: 'mail@auth-masterclass-tutorial.com',
+    to: email,
+    subject: 'Confirm your email',
+    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
+  });
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: existingToken.id }
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  });
+
+  return passwordResetToken;
+};
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: {
+        id: existingToken.id
+      }
+    });
+  }
+
+  const verficationToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  });
+
+  return verficationToken;
+};
