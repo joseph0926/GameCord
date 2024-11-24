62724c107da8741beebf8b7c92842826320b4852
joseph0926 <joseph0926@naver.com>
2024-11-24 15:12:58 +0900

    [CHORE] prisma 삭제

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,6 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
-import { prisma } from './lib/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [Google],
 });
